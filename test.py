84a77ca3a678c648eaf44bbd6e7c1778f8ab5233
kowangwang <a0975690163@gmail.com>
2022-09-01 16:14:44 +0800

    [add] youtube_info_project

--- test.py
+++
@@ -1,1 +0,0 @@
-print('test - pushing to my GitHub')
---
+++ youtube_info_project/youtube_info_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YoutubeInfoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'youtube_info_app'
