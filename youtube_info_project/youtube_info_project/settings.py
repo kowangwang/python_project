d415302682b73ce02a16d0c1af7046a9399560b8
kowangwang <a0975690163@gmail.com>
2022-09-01 21:50:22 +0800

    [fix] 暫存

--- youtube_info_project/youtube_info_project/settings.py
+++ youtube_info_project/youtube_info_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'youtube_info_app.apps.YoutubeInfoAppConfig'
 ]
 
 MIDDLEWARE = [
