e442dbbf053a5c856b1170bcafa10b8086925702
kowangwang <a0975690163@gmail.com>
2022-09-02 18:20:16 +0800

    [fix] views.py
    新增_show_description

--- youtube_info_project/youtube_info_app/views.py
+++ youtube_info_project/youtube_info_app/views.py
@@ -1,6 +1,22 @@
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from youtube_api_function import get_video_description
 
 def show_overview(request):
     template = loader.get_template('overview.html')
-    return HttpResponse(template.render())
+    context = {
+        "description":""
+    }
+
+    if 'video_url' in request.POST.keys():
+        video_url = request.POST['video_url']
+        _show_description(context, video_url)
+
+    return HttpResponse(template.render(context, request))
+
+def _show_description(context, video_url=''):
+    if video_url:
+        context['description'] = get_video_description(video_url)
+
+    return HttpResponseRedirect(reverse('show_overview'))
