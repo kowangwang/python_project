38ad49dd01c9bd4fa751c8456251f4d39fff7878
kowangwang <a0975690163@gmail.com>
2022-09-02 18:19:35 +0800

    [fix] urls.py

--- youtube_info_project/youtube_info_app/urls.py
+++ youtube_info_project/youtube_info_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_overview, name='index')
+    path('', views.show_overview, name='show_overview'),
+    path('get_video_desription/', views.show_overview, name='show_overview')
 ]
