f9eea191eca018eb47398641806f312abd9a115c
kowangwang <a0975690163@gmail.com>
2022-09-06 15:18:26 +0800

    [fix] urls.py
    加入新增, 刪除, 編輯資料相關的path

--- do_not_waste_project/do_not_waste_app/urls.py
+++ do_not_waste_project/do_not_waste_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_main_page, name='show_main_page')
+    path('', views.show_main_page, name='show_main_page'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord')
 ]
